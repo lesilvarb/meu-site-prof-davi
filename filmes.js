0d843f54bb35cb3aeb68cf32ca0500f68f40a6bb
lesilvarb <lesilvah2009@gmail.com>
2026-08-17 07:51:52 -0300

    atualizaçao
    atualizado

---
+++ filmes.js
@@ -0,0 +1,85 @@
+// filmes.js
+
+export const filmes = [
+    {
+        id: "interestelar",
+        titulo: "Interestelar",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
+        trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln"
+    },
+
+    {
+        titulo: "Batman",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Em Batman: O Cavaleiro das Trevas, a paz é ameaçada pelo Coringa, um anarquista que instaura o caos.",
+        trailer: "ID_YOUTUBE"
+    },
+
+    {
+        titulo: "Ong Bak",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Ong-Bak: Guerreiro Sagrado, acompanha Ting (Tony Jaa), um jovem guerreiro de uma pacata vila tailandesa.",
+        trailer: "ID_YOUTUBE"
+    },
+
+    {
+        titulo: "Kung-Fusão",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "A história acompanha Sing, um malandro trapalhão que tenta entrar para a temida Gangue do Machado.",
+        trailer: "ID_YOUTUBE"
+    },
+
+    {
+        titulo: "Homem Aranha",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "O jovem Peter Parker que, após ser picado por uma aranha, ganha superpoderes como força, agilidade e a capacidade de escalar paredes.",
+        trailer: "ID_YOUTUBE"
+    },
+
+    {
+        titulo: "Capitão América: Guerra Civil",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
+        trailer: "ID_YOUTUBE"
+    }
+];
+
+export const series = [
+    {
+        titulo: "Stranger Things",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Dark",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Dark é uma série focada em viagens no tempo e multiversos.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Breaking Bad",
+        imagem: "Imagens/imagem.jpg",
+        descricao: " A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Peaky Blinders",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Peaky Blinders acompanha Thomas Shelby e sua gangue em Birmingham após a Primeira Guerra Mundial.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Round 6",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
+        trailer: "ID_YOUTUBE"
+    },
+    {
+        titulo: "Avatar A lenda de Aang",
+        imagem: "Imagens/imagem.jpg",
+        descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
+        trailer: "ID_YOUTUBE"
+    }
+];
--- script.js
+++ script.js
@@ -1,55 +1,89 @@
-// FILME
-let filmes = [
+import { filmes, series } from "./filmes.js";    
+    
+    // =========================
+    // FUNÇÃO CRIAR CARD
+    // =========================
+    
+    function criarCard(item, tipo){
 
-    {
-    titulo: "RAPUNZEL",
-    imagem: "filme.jpg.webp",
-    descricao:" uma jovem de longos cabelos dourados aprisionada em uma torre alta por uma bruxa. Sem portas ou escadas, a torre só é acessível quando Rapunzel lança suas tranças. Ela anseia pela liberdade e é salva por um príncipe que ouve seu canto." 
+        return `
+            <div class="card">
+    
+                <img src="${item.imagem}" alt="${item.titulo}">
+    
+                <h3>${item.titulo}</h3>
+    
+                <p>${item.descricao}</p>
+    
+                <div class="botoes">
+    
+                    <a
+                        href="player.html?tipo=${tipo}&id=${item.id}"
+                        class="btnPlay"
+                    >
+                        ▶ Assistir
+                    </a>
+    
+                    <button onclick="favoritar('${item.titulo}')">
+                        + Minha Lista
+                    </button>
+    
+                </div>
+    
+            </div>
+        `;
     }
     
-    ];
     
-    // FILME
-    let filme = [
-    
-    {
-    titulo: "RATATOUILLE",
-    imagem: "filme.jpg",
-    descricao: "Remy, um rato francês com paladar refinado que sonha ser chef em Paris. Ele faz uma parceria improvável com Linguini, um ajudante de cozinha desajeitado, controlando seus movimentos sob o chapéu para criar pratos fantásticos no restaurante de seu ídolo, Gusteau."
-    }
-
-    
-    ];
+    // =========================
+    // MOSTRAR FILMES E SÉRIES
+    // =========================
     
     function mostrar(){
     
-    let listaFilmes = document.getElementById("listaFilmes");
-    let listaFilme = document.getElementById("listaFilme");
+        let listaFilmes = document.getElementById("listaFilmes");
+        let listaSeries = document.getElementById("listaSeries");
     
-    filmes.forEach(function(f){
+        
+        listaFilmes.innerHTML = "";
+        listaSeries.innerHTML = "";
     
-    listaFilmes.innerHTML += `
-    <div class="card">
-    <img src="${f.imagem}">
-    <h3>${f.titulo}</h3>
-    <p>${f.descricao}</p>
-    </div>
-    `;
     
-    });
-    
-    filme.forEach(function(s){
-    
-    listaFilme.innerHTML += `
-    <div class="card">
-    <img src="${s.imagem}">
-    <h3>${s.titulo}</h3>
-    <p>${s.descricao}</p>
-    </div>
-    `;
-    
-    });
+        filmes.forEach(function(f){
+            listaFilmes.innerHTML += criarCard(f, "filme");
+        });
+        
+        series.forEach(function(s){
+            listaSeries.innerHTML += criarCard(s, "serie");
+        });
     
     }
+    
+    
+    // =========================
+    // BOTÃO ASSISTIR
+    // =========================
+    
+    function assistir(nome){
+    
+        alert("▶ Reproduzindo: " + nome);
+    
+    }
+    
+    
+    // =========================
+    // FAVORITOS
+    // =========================
+    
+    function favoritar(nome){
+    
+        alert("⭐ Adicionado à sua lista: " + nome);
+    
+    }
+
+    window.favoritar = favoritar;
+    
+    
+    // INICIAR
     
     mostrar();
