a891d3f1beb1ede45abc8d8a31468b7618ec19d1
lesilvarb <lesilvah2009@gmail.com>
2026-08-31 07:53:57 -0300

    atualizacao

--- filmes.js
+++ filmes.js
@@ -10,13 +10,15 @@ export const filmes = [
     },
 
     {
-        titulo: "Uma ideia de voce",
+       id: "Uma ideia de voce",
+       titulo: "Uma ideia de voce",
         imagem: "Imagens/images.jpg",
         descricao: "Solène, uma mãe solo de 40 anos, começa um romance inesperado com Hayes Campbell, o jovem de 24 anos que é vocalista da boy band mais famosa do planeta.",
-        trailer: "mO7Tv67sE1I?si=gnTSOsNi2UzT5ous"
+        trailer: "hJ91gJLOKhk?si=rtyf6-0AcZ1tqTTu."
     },
 
     {
+        id: "Como eu era entes de voce",
         titulo: "Como eu era entes de voce",
         imagem: "Imagens/comoeuera.jpg",
         descricao: "Louisa Clark é contratada para cuidar de Will Traynor, um jovem rico e tetraplégico que perdeu o gosto pela vida, mudando o destino um do outro.",
@@ -24,62 +26,73 @@ export const filmes = [
     },
 
     {
+        id: "Para todos os garotos que ja amei",
         titulo: "Para todos os garotos que ja amei",
         imagem: "Imagens/paratodos.jpg",
         descricao: "Lara Jean escreve cartas de amor secretas para todos os garotos por quem já se apaixonou, mas sua vida vira de cabeça para baixo quando as cartas são misteriosamente enviadas.",
-        trailer: "ID_YOUTUBE"
+        trailer: "CK-g0OqzQHQ?si=LxxdSu0KMfftOszQ"
     },
 
     {
+        id: "A cinco passos de voce",
         titulo: "A cinco passos de voce",
         imagem: "Imagens/acincopassos.jpg",
         descricao: "Dois adolescentes que sofrem de fibrose cística se apaixonam em um hospital, mas são forçados a manter uma distância física segura para evitar infecções.",
-        trailer: "ID_YOUTUBE"
+        trailer: "5XeyXtTi8LE?si=aHzlcME2VMC4he-W"
     },
 
     {
+        id: "A culpa e das estrelas",
         titulo: "A culpa e das estrelas",
-        imagem: "Imagens/aculpaedas.jpg",
+        imagem: "imagens/aculpaedas.jpg",
         descricao: " Hazel Grace e Gus Waters são dois jovens pacientes com câncer que se conhecem em um grupo de apoio e compartilham uma jornada emocionante de amor e superação.",
-        trailer: "ID_YOUTUBE"
+        trailer: "L6fpXfQiC6g?si=pBLAI1c5sZj6EWLz"
     }
 ];
 
 export const series = [
     {
+
+        id: "O Verão Que Mudou Minha Vida",
         titulo: "O Verão Que Mudou Minha Vida",
         imagem: "Imagens/overao.webp",
         descricao: "Baseada nos livros de Jenny Han, acompanha um triângulo amoroso intenso durante as férias de verão na praia.",
-        trailer: ""
+        trailer: "AdiKOuBOy7Q?si=zJ9VbfdMS306q8kw"
     },
     {
+        id: "Sterling Point",
+        id: "Sterling Point",
         titulo: " Sterling Point",
         imagem: "Imagens/sterling.webp",
         descricao: " Annie e seu irmão gêmeo Connor descobrem cartas escondidas pelo pai e uma herança inesperada após a morte do avô materno no Canadá.",
-        trailer: "ID_YOUTUBE"
+        trailer: "bwwF3TuJQME?si=knBMWnbBSE61RZzp"
     },
     {
+        id: "Legalmente loira",
         titulo: "Legalmente loira",
         imagem: "Imagens/loira.jpg",
         descricao: " Longe de sua zona de conforto e de sua popularidade, ela precisa lidar com novos desafios sociais, dramas familiares, escolhas de moda e romances típicos da juventude",
-        trailer: "ID_YOUTUBE"
+        trailer: "I4GmyuiBkjc?si=_Y4x6MkEQm-e1u_T"
     },
     {
+        id: "Depois daquele ano",
         titulo: "Depois daquele ano",
         imagem: "Imagens/depois.jpg",
         descricao: "Peaky Blinders acompanha T Um erro grave no passado afasta os dois por anos, até que um reencontro inesperado os obriga a enfrentar sentimentos mal resolvidos.",
-        trailer: "ID_YOUTUBE"
+        trailer: "nyISb5s9ZlM?si=UTPf_0PDQ7NS9cbC"
     },
     {
-        titulo: "Round 6",
-        imagem: "Imagens/imagem.jpg",
-        descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
-        trailer: "ID_YOUTUBE"
+        id: "Chiquititas",
+        titulo: "Chiquititas",
+        imagem: "Imagens/chiquititas.jpg",
+        descricao: "A trama gira em torno de crianças órfãs ou abandonadas que encontram na amizade, na música e no apoio da doce cuidadora Carolina (Carol) uma verdadeira família.",
+        trailer: "vOMa-PzUdKg?si=Dcnqi3FzyFeuXr3C"
     },
     {
-        titulo: "Avatar A lenda de Aang",
-        imagem: "Imagens/imagem.jpg",
+        id: "Barbie",
+        titulo: "Barbie",
+        imagem: "Imagens/barbie.jpg",
         descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
-        trailer: "ID_YOUTUBE"
+        trailer: "okss5sUpca8?si=sR9LN2O0yRHOlTO-"
     }
 ];
